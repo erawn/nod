5b15915aca4b8d16d962658bf3af8dd3c11275af
Eric Rawn <erawn@berkeley.edu>
2026-01-14 13:05:08 -0800

    format, add call position to nodinfo

--- test.py
+++ test.py
@@ -3,18 +3,21 @@ import ipdb
 def caller(\
         ):
     a = 15
+
+
+    def baller(a, \
+            b, c):
+        b = 20
+
+        c = 40
+        print('\n')
+
+        for x in range(0,10):
+
+            c+=1
+
+        nod()
     baller(a, 10, 20)
-
-def baller(a, \
-           b, c):
-    b = 20
-
-    c = 40
-
-    for x in range(0,10):
-        c+=1
-        
-    nod()
 
 
 global a 
